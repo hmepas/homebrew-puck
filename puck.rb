1454c1640fa048ade199dad98edab5ee3777d890
Pavel Khmelinskiy <p.khmelinskiy@samolet.ru>
2025-04-17 01:20:52 +0300

    feat: update to v0.1.8 - Update description and binary name

--- puck.rb
+++ puck.rb
@@ -1,16 +1,16 @@
 class Puck < Formula
   desc "Swift Input Method Switching Daemon for macOS"
   homepage "https://github.com/hmepas/puck"
-  url "https://github.com/hmepas/puck/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "a16585ea8bf868c56b8c6bc485ebc767043f8f08c7284b67f2aeacfc1ee23648"
+  url "https://github.com/hmepas/puck/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "d748a9f48bad824301951a2dddee7dfc6417506b593287acf24daea1f6af08c4"
   license "MIT"
 
   depends_on :macos
   depends_on xcode: ["14.0", :build]
 
   def install
-    system "swift", "build", "--configuration", "release", "--disable-sandbox"
-    bin.install ".build/release/Puck"
+    system "swift", "build", "--disable-sandbox", "-c", "release"
+    bin.install ".build/release/puck"
   end
 
   def post_install
@@ -20,6 +20,6 @@ class Puck < Formula
   end
 
   test do
-    system "#{bin}/Puck", "--version"
+    system "#{bin}/puck", "--version"
   end
 end
