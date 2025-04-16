cb78c8d4f0af1b8c260ab3d7ce4d41e80d96e495
Pavel Khmelinskiy <p.khmelinskiy@samolet.ru>
2025-04-17 00:33:10 +0300

    feat: add Puck formula

---
+++ puck.rb
@@ -0,0 +1,24 @@
+class Puck < Formula
+  desc "Swift Input Method Switching Daemon for macOS"
+  homepage "https://github.com/hmepas/puck"
+  url "https://github.com/hmepas/puck/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "307e63f4dd5cb6780e93ee88dbc40a128af42a93e7409b22fe391c738a60d361"
+  license "MIT"
+
+  depends_on :macos
+  depends_on xcode: ["14.0", :build]
+
+  def install
+    system "swift", "build", "--configuration", "release", "--disable-sandbox"
+    bin.install ".build/release/Puck"
+  end
+
+  def post_install
+    # Ensure config directory exists
+    (ENV["HOME"] + "/.config/puck").mkpath
+  end
+
+  test do
+    system "#{bin}/Puck", "--version"
+  end
+end 
